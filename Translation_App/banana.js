fcf66b5f354eef58a236ba4a746e1bb9eebed179
Harish-port <harish.sq@gmail.com>
2022-12-09 23:21:47 +0530

    translator latest changes

--- Translation_App/banana.js
+++
@@ -1,7 +0,0 @@
-var inputTxt = document.querySelector("#input-txt");
-var btnTranslate = document.querySelector("#translate");
-console.log(btnTranslate);
-function clickHandler(){
-    console.log( inputTxt.value,"testing fucntion");
-}
-btnTranslate.addEventListener('click',clickHandler);
